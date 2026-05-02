2730556b520a7778575310c2acb4d3b51a42b1ae
Snehil <snehilsingh7800m@gmail.com>
2026-05-02 14:37:55 +0530

    added fooer

--- vita/context_processors.py
+++ vita/context_processors.py
@@ -1,6 +1,24 @@
-from .models import PrimaryCategory
+from .models import PrimaryCategory,Blog, CalcUsage
 
 def global_categories(request):
     return {
         'primary_categories': PrimaryCategory.objects.all()
+    }
+from django.utils.timezone import now
+
+def site_stats(request):
+    current_month = now().month
+    total_blogs = Blog.objects.filter(status='published').count()
+
+    total_calculators = 11  # static ya baad me dynamic kar sakte
+
+    #monthly_users = CalcUsage.objects.count()  # total usage
+    monthly_users = CalcUsage.objects.filter(
+    created_at__month=current_month
+    ).count()
+
+    return {
+        'total_blogs': total_blogs,
+        'total_calculators': total_calculators,
+        'monthly_users': monthly_users
     }
--- vitarthi/settings.py
+++ vitarthi/settings.py
@@ -76,6 +76,7 @@ TEMPLATES = [
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
                 'vita.context_processors.global_categories',
+                'vita.context_processors.site_stats',
             ],
         },
     },
