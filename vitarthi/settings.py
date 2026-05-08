d1b949f901db27af0115a61b51646509de0c7f51
Snehil <snehilsingh7800m@gmail.com>
2026-05-08 11:05:32 +0530

    modified

--- vitarthi/settings.py
+++ vitarthi/settings.py
@@ -155,6 +155,7 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 AUTH_USER_MODEL = 'vita.CustomUser'
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
